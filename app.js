d851da1206c8c9c6abc56442cd02c3dd4c32ae92
Yoonha Kim <ivryxx@gmail.com>
2022-04-30 20:56:25 +0900

    signUp/login success

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-const routers = express("./routes");
+const routers = require("./routes/logins");
 
 const app = express();
-const port = 7000;
+// const port = 7000;
 
 //접속로그 확인
 // const requestMiddleware = (req, res, next) => {
@@ -17,7 +17,7 @@ const port = 7000;
 // };
 
 //미들웨어
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 // app.use(requestMiddleWare);
 app.use(express.urlencoded({ extended: false }));
@@ -25,8 +25,8 @@ app.use(express.urlencoded({ extended: false }));
 //라우터 연결
 app.use("/", routers);
 
-app.use(express.static("assets"));
+// app.use(express.static("assets"));
 
-app.listen(7000, () => {
+app.listen(3000, () => {
   console.log("서버가 요청을 받을 준비가 됐어요");
 });
--- routes/logins.js
+++ routes/logins.js
@@ -1,37 +1,123 @@
 const express = require("express");
 const router = express.Router();
-const {
-  signUp,
-  idCheck,
-  nameCheck,
-  getUser,
-  logOut,
-} = require("../controller/login");
-const authMiddleware = require("../middlewares/auth-middleware");
+const { User } = require("../models");
+const { Op } = require("sequelize");
+const jwt = require("jsonwebtoken");
 
 //회원가입
-router.post("/signUp", signUp);
+router.post("/signUp", async (req, res) => {
+  console.log(1);
+  const {
+    userEmail,
+    pwd,
+    pwdCheck,
+    userName,
+    userType,
+    userProfile,
+    tag,
+    contents,
+  } = req.body;
+  console.log(2);
+  //비밀번호 최소 문자 1, 숫자 1 포함 (8자리 이상) 정규식
+  // const pwdValidation = /^(?=.*[A-Za-z])(?=.*\d)[\w]{8,}$/;
 
+  // if (!pwdValidation.test(pwd)) {
+  //   res.status(400).send({
+  //     errorMessage: "비밀번호는 영문+숫자 조합으로 8자리 이상 사용해야합니다.",
+  //   });
+  //   return;
+  // }
+  if (pwd !== pwdCheck) {
+    res.status(400).send({
+      errorMessage: "패스워드가 패스워드 확인란과 동일하지 않습니다.",
+    });
+    return;
+  }
+  const existUser = await User.findAll({
+    where: {
+      [Op.or]: [{ userName }, { userEmail }],
+    },
+  });
+  if (existUser.length) {
+    res.status(400).send({
+      errorMessage: "이미 등록된 아이디 또는 이메일입니다.",
+    });
+    return;
+  }
+  await User.create({
+    userEmail,
+    pwd,
+    userName,
+    userType,
+    userProfile,
+    tag,
+    contents,
+  });
+  res.status(201).send({});
+});
+console.log(3);
 //아이디 중복 검사
-router.post("/login/idCheck", idCheck);
+router.post("/signUp/emailCheck", async (req, res) => {
+  const existUser = await User.findAll({
+    where: { userEmail },
+  });
+  if (existUser.length) {
+    res.status(400).send({
+      errorMessage: "이미 등록된 아이디입니다.",
+    });
+    return;
+  }
+});
 
-//이름 중복 검사
-router.post("/login/nameCheck", nameCheck);
+//닉네임 중복 검사
+router.post("/signUp/nameCheck", async (req, res) => {
+  const existUser = await User.findAll({
+    where: { userEmail },
+  });
+
+  if (existUser.length) {
+    res.status(400).send({
+      errorMessage: "이미 등록된 닉네임입니다.",
+    });
+    return;
+  }
+});
 
 //로그인
-router.post("/login", login);
+router.post("/login", async (req, res) => {
+  // const login = async (req, res) => {
+  const { userEmail, pwd } = req.body;
+  const user = await User.findOne({ where: { userEmail, pwd } });
+
+  if (!user) {
+    res.status(400).send({
+      errorMessage: "닉네임 또는 패스워드를 확인해주세요.",
+    });
+    return;
+  }
+  const token = jwt.sign({ userId: user.userId }, "my-secret-key");
+  res.send({
+    token,
+  });
+});
 
 //유저 정보 불러오기
-router.get("/login/getUser", authMiddleware, (req, res) => {
+router.get("/login/getUser", (req, res) => {
   const { user } = res.locals;
   console.log(user);
   res.json(user);
 });
 
 //로그아웃
-router.get("/login/logOut", logOut);
+// router.get("/login/logOut", logOut);
 
-// 사용자 인증
-// router.get("/users/me", middleswares, user);
+//   //사용자 인증 미들웨어
+//   const user = async (req, res) => {
+//     const { user } = res.locals;
+//     res.send({
+//       user,
+//     });
+//     };
+// });
 
 module.exports = router;
