c02771ef1fc4f4496796036fda6419a6b34a12e9
Yoonha Kim <ivryxx@gmail.com>
2022-05-04 11:27:14 +0900

    updated

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,24 +1,24 @@
-const jwt = require("jsonwebtoken");
-const { User } = require("../models");
+const jwt = require('jsonwebtoken');
+const {User} = require('../models');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  const [authType, authToken] = (authorization || "").split(" ");
-  if (!authToken || authType !== "Bearer") {
+  const {authorization} = req.headers;
+  const [authType, authToken] = (authorization || '').split(' ');
+  if (!authToken || authType !== 'Bearer') {
     res.status(401).send({
-      errorMessage: "로그인 후 이용 가능한 기능입니다.",
+      errorMessage: '로그인 후 이용 가능한 기능입니다.',
     });
     return;
   }
 
   try {
-    const { userId } = jwt.verify(authToken, "my-secret-key");
-    console.log({ userId });
-    User.findByPk({ userId }).then((user) => {
+    const {userId} = jwt.verify(authToken, 'my-secret-key');
+    console.log(userId);
+    User.findByPk(userId).then(user => {
       res.locals.user = user;
       next();
     });
   } catch (error) {
-    return res.status(401).send({ errorMessage: "로그인 후 사용하세요." });
+    return res.status(401).send({errorMessage: '로그인 후 사용하세요.'});
   }
 };
