7728378544ceab2fc80c40e193fd28feabc65210
jeongmin <101089290+jeongminGit@users.noreply.github.com>
2022-05-07 22:01:13 +0900

    port 3000 check

--- app.js
+++ app.js
@@ -48,30 +48,30 @@ const https = require("https")
 const SocketIO = require("socket.io")
 
 
-//////////////////////////////////////////////////////////////////
-// https 인증관련
-const httpPort= 80;
-const httpsPort = 443;
-const privateKey = fs.readFileSync(__dirname + '/private.key', 'utf8');
-const certificate = fs.readFileSync(__dirname + '/certificate.crt', 'utf8');
-const ca = fs.readFileSync(__dirname + '/ca_bundle.crt', 'utf8');
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca,
-};
+// //////////////////////////////////////////////////////////////////
+// // https 인증관련
+// const httpPort= 80;
+// const httpsPort = 443;
+// const privateKey = fs.readFileSync(__dirname + '/private.key', 'utf8');
+// const certificate = fs.readFileSync(__dirname + '/certificate.crt', 'utf8');
+// const ca = fs.readFileSync(__dirname + '/ca_bundle.crt', 'utf8');
+// const credentials = {
+//   key: privateKey,
+//   cert: certificate,
+//   ca: ca,
+// };
 
-const httpServer = http.createServer(app_low);
-const httpsServer = https.createServer(credentials,app)
+// const httpServer = http.createServer(app_low);
+// const httpsServer = https.createServer(credentials,app)
 
-// socket.io https 서버
-const io = SocketIO(httpsServer, {
-	cors: {
-		origin: "*",
-		methods: [ "GET", "POST" ]
-	}
-});
-/////////////////////////////////////////////////////////
+// // socket.io https 서버
+// const io = SocketIO(httpsServer, {
+// 	cors: {
+// 		origin: "*",
+// 		methods: [ "GET", "POST" ]
+// 	}
+// });
+// /////////////////////////////////////////////////////////
 
 // const helmet = require('helmet') 
 // app.use(helmet());
@@ -88,19 +88,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-// https 인증관련
-app.get('/.well-known/pki-validation/69DCB230704B206B1161AA5BC7E57864.txt', (req,res)=>{
-	res.sendFile(__dirname + '/well-known/pki-validation/69DCB230704B206B1161AA5BC7E57864.txt')
-  });
-app_low.use((req,res,next)=>{
-	if(req.secure){
-	  next();
-	}else{
-	  const to = `https://${req.hostname}:${httpsPort}${req.url}`;
-	  console.log(to);
-	  res.redirect(to)
-	}
-  })
+// // https 인증관련
+// app.get('/.well-known/pki-validation/69DCB230704B206B1161AA5BC7E57864.txt', (req,res)=>{
+// 	res.sendFile(__dirname + '/well-known/pki-validation/69DCB230704B206B1161AA5BC7E57864.txt')
+//   });
+// app_low.use((req,res,next)=>{
+// 	if(req.secure){
+// 	  next();
+// 	}else{
+// 	  const to = `https://${req.hostname}:${httpsPort}${req.url}`;
+// 	  console.log(to);
+// 	  res.redirect(to)
+// 	}
+//   })
 //라우터 연결
 app.use("/", loginRouter, reservationRouter, getLikeRouter, authRouter, reviewRouter);
 
@@ -116,77 +116,77 @@ app.get('/', function (req, res) {
 // });
 
 
-// app.listen(3000, () => {
-//   console.log("3000번 포트에서 대기중!");
-// });
+app.listen(3000, () => {
+  console.log("3000번 포트에서 대기중!");
+});
 
-io.on("connection", (socket) => {
-	console.log(1)
-	// socket.emit("me", socket.id);
-	socket.on('joinRoom',(roomName)=>{
-		let rooms = io.sockets.adapter.rooms;
-    	let room = rooms.get(roomName);
-		console.log(2)
+// io.on("connection", (socket) => {
+// 	console.log(1)
+// 	// socket.emit("me", socket.id);
+// 	socket.on('joinRoom',(roomName)=>{
+// 		let rooms = io.sockets.adapter.rooms;
+//     	let room = rooms.get(roomName);
+// 		console.log(2)
 		
-		if (room == undefined) {
-			console.log(2.1)
-			socket.join(roomName);
-			socket.emit("created");
-			console.log(2.2)
+// 		if (room == undefined) {
+// 			console.log(2.1)
+// 			socket.join(roomName);
+// 			socket.emit("created");
+// 			console.log(2.2)
 			
-		  } else if (room?.size == 1) {
-			  console.log(2.3)
-			//room.size == 1 when one person is inside the room.
-			socket.join(roomName);
-			socket.emit("joined");
-			console.log(2.4)
+// 		  } else if (room?.size == 1) {
+// 			  console.log(2.3)
+// 			//room.size == 1 when one person is inside the room.
+// 			socket.join(roomName);
+// 			socket.emit("joined");
+// 			console.log(2.4)
 			
-		  } else {
-			//when there are already two people inside the room.
-			socket.emit("full");
-			console.log(2.5)
-		  }
+// 		  } else {
+// 			//when there are already two people inside the room.
+// 			socket.emit("full");
+// 			console.log(2.5)
+// 		  }
 		  
-		//   console.log(rooms);
-		});
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	});
-	socket.on("ready", function (roomName) {
-		socket.broadcast.to(roomName).emit("ready"); //Informs the other peer in the room.
-	  });
+// 		//   console.log(rooms);
+// 		});
+// 	socket.on("disconnect", () => {
+// 		socket.broadcast.emit("callEnded")
+// 	});
+// 	socket.on("ready", function (roomName) {
+// 		socket.broadcast.to(roomName).emit("ready"); //Informs the other peer in the room.
+// 	  });
 
 
-	socket.on('sendingSignal',({signal, roomName})=>{
-		console.log(3)
-		console.log({signal,roomName})
-		io.to(roomName).emit("offer",{signal, roomName, stream})
-		console.log(3.5)
+// 	socket.on('sendingSignal',({signal, roomName})=>{
+// 		console.log(3)
+// 		console.log({signal,roomName})
+// 		io.to(roomName).emit("offer",{signal, roomName, stream})
+// 		console.log(3.5)
 		
-	  })
-	//   socket.on(‘sendingSignal’,({signal, roomName})=>{
-    //     console.log(3)
-    //     io.to(roomName).emit(“offer”, {signal, roomName, stream})
-    //     console.log(3.5)
-    //   })
+// 	  })
+// 	//   socket.on(‘sendingSignal’,({signal, roomName})=>{
+//     //     console.log(3)
+//     //     io.to(roomName).emit(“offer”, {signal, roomName, stream})
+//     //     console.log(3.5)
+//     //   })
 
 	  
-	socket.on("returningSignal", ({ signal, roomName }) => {
-		console.log({signal,roomName})
-		console.log(4)
-		io.to(roomName).emit("receivingSignal", signal)
-		// io.to(roomName).emit("answer", { signal: signal});
-		console.log(4.5)
-	});
-})
+// 	socket.on("returningSignal", ({ signal, roomName }) => {
+// 		console.log({signal,roomName})
+// 		console.log(4)
+// 		io.to(roomName).emit("receivingSignal", signal)
+// 		// io.to(roomName).emit("answer", { signal: signal});
+// 		console.log(4.5)
+// 	});
+// })
 
 
 
 
 
-httpServer.listen(httpPort, ()=>{
-	console.log('http서버가 켜졌어요');
-  });
-httpsServer.listen(httpsPort,() =>{
-	console.log('https서버가 켜졌어요')
-});
+// httpServer.listen(httpPort, ()=>{
+// 	console.log('http서버가 켜졌어요');
+//   });
+// httpsServer.listen(httpsPort,() =>{
+// 	console.log('https서버가 켜졌어요')
+// });
