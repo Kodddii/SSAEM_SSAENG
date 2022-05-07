3949eceb7c61d159e4e047228488a24c66d499d0
jeongmin <101089290+jeongminGit@users.noreply.github.com>
2022-05-07 19:11:28 +0900

    review draft

--- routes/logins.js
+++ routes/logins.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const {User} = require('../models');
-const {Op} = require('sequelize');
 const jwt = require('jsonwebtoken');
 
 //회원가입
--- routes/review.js
+++ routes/review.js
@@ -4,11 +4,11 @@ const db = require('../config');
 // const authMiddleare = require('../middlewares/auth');
 ///
 // 리뷰 불러오기(조회)
-router.get('/getReview/:tutor_name', async (req, res) => {
-  const { tutor_name } = req.params;
+router.get('/getReview/:tutor_userName', async (req, res) => {
+  const { tutor_userName } = req.params;
   console.log(req.params);
-  const sql = "SELECT * FROM review WHERE tutor_name=?"
-  db.query(sql, [tutor_name], (err, datas) => {
+  const sql = "SELECT * FROM Review WHERE tutor_userName=?"
+  db.query(sql, [tutor_userName], (err, datas) => {
     if (err) {
       console.log(err);
       res.send({ meg: 'fail' })
@@ -21,15 +21,15 @@ router.get('/getReview/:tutor_name', async (req, res) => {
 
 // 리뷰 작성
 router.post('/addReview', async (req, res) => {
-  // const { tutee_name, rate, text } = req.body;
+  // const { tutee_userName, rate, text } = req.body;
   // const { token } = res.locals;
-  const { tutor_name, tutee_name, tutee_profile, rate, text } = req.body;
-  const reviewId = Math.floor(Math.random() * (2147483647)) + 1;
-  console.log(reviewId, req.body);
-  const param = [reviewId, tutor_name, tutee_name, tutee_profile, rate, text];
+  const { tutor_userName, tutee_userName, rate, text } = req.body;
+  // const reviewId = Math.floor(Math.random() * (2147483647)) + 1;
+  console.log(req.body);
+  const param = [tutor_userName, tutee_userName, rate, text];
   console.log(param)
   db.query(
-    'INSERT INTO `review`(`reviewId`, `tutor_name`, `tutee_name`, `tutee_profile`, `rate`, `text`) VALUES (?,?,?,?,?,?)',
+    'INSERT INTO `Review`(`tutor_userName`, `tutee_userName`, `rate`, `text`) VALUES (?,?,?,?)',
     param,
     (err, row) => {
       if (err) {
@@ -44,12 +44,12 @@ router.post('/addReview', async (req, res) => {
 // 리뷰 수정
 router.patch('/editReview', async (req, res) => {
   // const { token } = res.locals;
-  const { tutee_name, reviewId, rate, text } = req.body;
-  const sql = 'SELECT * FROM review WHERE tutee_name=?'
-  db.query(sql, [tutee_name], (err, rows) => {
+  const { tutee_userName, reviewId, rate, text } = req.body;
+  const sql = 'SELECT * FROM Review WHERE tutee_userName=?'
+  db.query(sql, [tutee_userName], (err, rows) => {
     if (rows.length !== 0) {
       // const sql = 'SELECT * FROM review WHERE reviewId=?'
-      const sql = 'UPDATE review SET rate=?, text=? WHERE reviewId=?'
+      const sql = 'UPDATE Review SET rate=?, text=? WHERE reviewId=?'
       db.query(sql, [rate, text, reviewId], (err, datas) => {
         if (err) {
           console.log(err)
@@ -64,12 +64,12 @@ router.patch('/editReview', async (req, res) => {
 // 리뷰 삭제
 router.delete('/deleteReview', async (req, res) => {
   // const { token } = res.locals;
-  const { tutee_name, reviewId } = req.body;
-  const sql = 'SELECT * FROM review WHERE tutee_name=?'
-  db.query(sql, [tutee_name], (err, rows) => {
+  const { tutee_userName, reviewId } = req.body;
+  const sql = 'SELECT * FROM Review WHERE tutee_userName=?'
+  db.query(sql, [tutee_userName], (err, rows) => {
     if (rows.length !== 0) {
       console.log(rows)
-      const sql = 'DELETE FROM review WHERE reviewId=?'
+      const sql = 'DELETE FROM Review WHERE reviewId=?'
 
       db.query(sql, [reviewId], (err, data) => {
         if (data == undefined) {
