095097c35b4eefab15fa56c513ae816dc5ca998d
Kodddii <justic521@gmail.com>
2022-05-19 16:13:11 +0900

    Update app.js

--- app.js
+++ app.js
@@ -278,7 +278,7 @@ io.on('connection', (socket) => {
 // });
 
 httpServer.listen(httpPort, ()=>{
-	console.log('http서버가 켜졌어요 이게맞나요');
+	console.log('http서버가 켜졌어요 이게맞나요 제발제발');
 });
 httpsServer.listen(httpsPort,() =>{
 	console.log('https서버가 켜졌어요 이거맞나요?')
