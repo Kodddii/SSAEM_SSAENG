d72be176169f02b5fc37b51376035a49f910d4b2
Kodddii <justic521@gmail.com>
2022-05-24 01:39:19 +0900

    Update app.js

--- app.js
+++ app.js
@@ -134,17 +134,17 @@ io.on('connection', (socket) => {
 	console.log(2.1)
 	io.to(roomId).emit('user-connected', userId);
 	console.log(2.2)
-	socket.on('send_message', (data) => {
-		console.log(data)
-		socket.to(roomId).emit('receive_message', data);
-		console.log(5)
-	});
+	
 	socket.on('disconnect', () => {
 		console.log(3)
 		io.to(roomId).emit('user-disconnected', userId);
 		console.log(3.5)
+	});	
+	socket.on('send_message', ({message,room,author}) => {
+		console.log({message,room,author})
+		socket.to(room).emit('receive_message',author, message);
+		console.log(5)
 	});
-	
 
 });
 	
