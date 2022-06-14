339830763109f6af7071ffd0356248c0a69423a5
Kodddii <justic521@gmail.com>
2022-06-14 23:12:33 +0900

    getLike, reservation controller 추가

---
+++ controller/reservationController.js
@@ -0,0 +1,250 @@
+require("moment-timezone")
+const db = require("../config")
+const moment = require("moment");
+moment.tz.setDefault("Asia/Seoul")
+//예약
+const addBooking = (req,res)=>{
+    const {end, start, userName} = req.body;
+    try {
+        const bookingMoment = new moment()
+        const startMoment = moment(start)
+        const time = moment.duration(startMoment.diff(bookingMoment)).asMinutes()
+        console.log(time)
+        if(time<60){
+            res.status(400).send({msg:'수업시작 1시간 전까지만 예약가능합니다.'})
+            return;
+        }
+        const start2 = start.replace(' (대한한국 표준시)','')
+        const end2= end.replace(' (대한한국 표준시)','')
+        const tutorName = req.params.tutorName
+        const datas = [start2,end2,tutorName,userName]
+        const sql0 = 'SELECT * FROM TimeTable WHERE start=? AND end=? AND Tutor_userName=? AND Tutee_userName=?'
+        const sql = 'INSERT INTO TimeTable (`start`,`end`,`Tutor_userName`,`Tutee_userName`) VALUES (?,?,?,?)'
+        const sqlUpdate = 'UPDATE Tutee SET `bookingCnt`= `bookingCnt`+1 WHERE userName=?'
+        const sqlSelectCnt = 'SELECT bookingCnt FROM Tutee WHERE userName=?'
+        const ansSql = userName
+        db.query(sqlSelectCnt , ansSql, (err,data)=>{
+            if(err) console.log(err)
+            else if(data[0].bookingCnt>5){
+                res.status(400).send({msg:'하루에 5타임이상 예약할수 없습니다.'})
+            }else{
+                db.query(sql0 , datas, (err,rows)=>{
+                    if(err){
+                        console.log(err)
+                    }else if (rows.length){
+                        res.status(400).send({msg:'이미 예약되어있는 시간입니다.'})
+                    }else if (!rows.length){
+                        db.query(sql,datas,(err,rows)=>{
+                            if (err) {
+                                console.log(err);
+                                res.status(400).send({ msg: 'fail' });
+                            } else {
+                                db.query(sqlUpdate,ansSql,(err,data)=>{
+                                    if(err){
+                                        console.log(err)
+                                    }else{
+                                        console.log('update success')
+                                    }
+                                })
+                                res.status(201).send({ msg: 'success' });
+                            }
+                        })
+                        
+                    }
+                })
+            }
+        })
+
+        
+    } catch (error) {
+        
+    }
+}
+
+// 예약된 리스트 불러오기
+const getBooking = (req,res)=>{
+    console.log(req.query)
+    const userId = req.query.userId
+    const isTutor = req.query.isTutor
+    const userName = req.query.userName
+    console.log(typeof isTutor)
+    if (isTutor==='1'){
+        const sql1 ='SELECT * FROM TimeTable WHERE Tutor_userName=? '
+        db.query(sql1, userName, (err,datas0)=>{
+        if(err) {
+            console.log(err);
+        }else{
+            // 데이터 값 시간순 정렬
+            const datas1  = datas0.sort((a,b) => new moment(a.start).format('x') - new moment(b.start).format('x'))
+            res.status(201).send({msg:'success', datas1})
+        }
+    })
+    }else if(isTutor==='0'){
+        const sql2 ='SELECT * FROM TimeTable WHERE Tutee_userName=?'
+        db.query(sql2, userName, (err,datas0)=>{
+        if(err) {
+            console.log(err);
+        }else{
+            const datas1  = datas0.sort((a,b) => new moment(a.start).format('x') - new moment(b.start).format('x'))
+            res.status(201).send({msg:'success', datas1})
+        }
+    })
+    }
+}
+
+// 알림용 예약리스트 불러오기
+const getNoti = (req,res)=>{
+    const user = res.locals.user
+    
+    if(user.isTutor === 1){
+        const sql = 'SELECT * FROM TimeTable WHERE Tutor_userName=? AND (TutorNoti = ? OR TutorDel=?) ORDER BY createdAt DESC'
+        db.query(sql,[user.userName,1,1],(err,data)=>{
+            if(err) console.log(err)
+            else{
+                // const data  = data0.sort((a,b) => new moment(a.start).format('x') - new moment(b.start).format('x'))
+                res.status(200).send(data);
+            }
+        })
+    }else if(user.isTutor===0){
+        const sql = 'SELECT * FROM TimeTable WHERE Tutee_userName=? AND ( TuteeNoti = ? OR  TuteeDel=?) ORDER BY createdAt DESC'
+        db.query(sql,[user.userName,1,1],(err,data)=>{
+            if(err) console.log(err);
+            else{
+                // const data  = data0.sort((a,b) => new moment(a.start).format('x') - new moment(b.start).format('x'))
+                res.status(200).send(data)
+            }
+        })
+    }
+}
+
+// 알림 리스트에서 삭제 
+const delNoti = (req,res)=>{
+    const user = res.locals.user
+    console.log(req.query)
+    const {timeId} =req.query
+    if(user.isTutor===1){
+        const sql = 'UPDATE TimeTable SET TutorNoti = ?  WHERE timeId=? '
+        const answer = [0,parseInt(timeId)]
+        db.query(sql,answer, (err,data)=>{
+            if(err) {
+                console.log(err);
+                res.status(400).send({msg:'update failed'})
+            }
+            else{
+                res.status(200).send({msg:'update success!'});
+            }
+        }) 
+    }else if(user.isTutor===0){
+        const sql = 'UPDATE TimeTable SET TuteeNoti = ?  WHERE timeId=? '
+        const answer = [0,parseInt(timeId)]
+        db.query(sql,answer, (err,data)=>{
+            if(err) {
+                console.log(err);
+                res.status(400).send({msg:'update fail'})
+            }
+            else{
+                res.status(200).send({msg:'update success!'});
+            }
+        }) 
+    }
+}
+
+// 예약 취소하기
+const delBooking = (req,res)=>{
+    const user = res.locals.user
+    const {timeId} = req.query
+    if(user.isTutor===1){
+        const sql0 = 'SELECT start FROM TimeTable WHERE timeID =?'
+        db.query(sql0, timeId, (err,data)=>{
+            if(err){
+                console.log(err)
+                return;
+            }
+            
+            const moment = require("moment")
+            const startTime=moment(data[0].start);
+            const cancelMoment = new moment();
+            const time = moment.duration(startTime.diff(cancelMoment)).asMinutes()
+            if(time<60){
+                res.status(400).send({msg:'예약취소는 한시간 전까지만 가능합니다.'})
+            }else if(time>=60){
+                const sql = 'UPDATE TimeTable SET TuteeDel = ? WHERE timeId =? '
+                const answer = [1,parseInt(timeId)]
+                db.query(sql, answer, (err,data)=>{
+                    if(err){
+                        console.log(err);
+                        res.status(400).send({msg:'fail'})
+                    } 
+                    else{
+                        res.status(200).send({msg:'success'})
+                    }
+                })
+            }    
+        })
+        
+    }else if(user.isTutor===0){
+        const sql0 = 'SELECT start FROM TimeTable WHERE timeID =?'
+        db.query(sql0,timeId, (err,data)=>{
+            if(err){
+                console.log(err);
+                res.status(400).send({msg:'에러입니다'})
+            }
+            const moment = require("moment")
+            const startTime=moment(data[0].start);
+            const cancelMoment = new moment();
+            const time = moment.duration(startTime.diff(cancelMoment)).asMinutes()
+            if(time<60){
+                res.status(400).send({msg:'예약취소는 한시간 전까지만 가능합니다.'})
+            }else if(time>=60){
+                const sql = 'UPDATE TimeTable SET TutorDel = ? WHERE timeId =? '
+                const answer = [1,parseInt(timeId)]
+                db.query(sql, answer, (err,data)=>{
+                    if(err){
+                        console.log(err);
+                        res.status(400).send({msg:'fail'})
+                    } 
+                    else{
+                        res.status(200).send({msg:'success'})
+                    }
+                })
+            }
+        })
+    }
+}
+
+// 예약 취소 삭제 확정
+const delBookingCheck = (req,res)=>{
+    const user = res.locals.user
+    const {timeId} = req.query
+    const sql = 'DELETE FROM TimeTable WHERE timeId=?'
+    db.query(sql, parseInt(timeId), (err,data)=>{
+        if(err) console.log(err);
+        else{
+            res.status(200).send({msg:'Delete complete'})
+        }
+    })
+}
+
+// 알림전체제거
+const delAllNoti = (req,res)=>{
+    const user = res.locals.user;
+    console.log(req.body)
+    const timeIdArray = req.body;
+    for (let timeId of timeIdArray){
+        const sql = 'UPDATE TimeTable SET noti = ? WHERE timeId=?'
+        const answer = [0,parseInt(timeId)]
+        db.query(sql,answer,(err,data)=>{
+            if(err) console.log(err)
+        })
+    }
+    res.status(200).send({msg:'success'})
+}
+module.exports ={ 
+    addBooking, 
+    getBooking, 
+    getNoti,
+    delNoti,
+    delBooking,
+    delBookingCheck,
+    delAllNoti,
+}
--- routes/getLike.js
+++ routes/getLike.js
@@ -4,255 +4,43 @@ const router = express.Router();
 const db = require('../config');
 const res = require('express/lib/response');
 const { query } = require('../config');
+const {
+    like,
+    unlike,
+    getPopularTutor,
+    getTutor,
+    getTutorTag,
+    getTag,
+    getUserDetail,
+    getLikeList,
+    isLike,
+} = require("../controller/getLikeController")
 
 // Like
-router.patch('/like',authMiddleware,(req,res)=>{
-    const user = res.locals.user
-    const {tutorName} = req.body;
-    if(user.isTutor===1){
-        res.status(400).send({msg:'fail'})
-        return;
-    }
-    if(user.userName===tutorName) {
-        res.status(400).send({msg:'fail'})
-        return;
-    }
-    const sql0 = 'SELECT * FROM `Like` WHERE Tutee_userName=? AND Tutor_userName=?'
-    const sql1 =  'UPDATE Tutor SET `like` = `like` + 1 WHERE userName=?'
-    const answerData = [user.userName, tutorName]
-    db.query(sql0, answerData , (err,data0)=>{
-        if(err) {
-            console.log(err)
-
-        }else if(data0.length){
-            console.log(data0.length)
-            res.status(400).send({msg: '이미 like한 Tutor 입니다'})
-        }else{
-            db.query(sql1,tutorName,(err,rows1)=>{
-                if(err){
-                    console.log(err)
-                }else{
-                    console.log('success')
-                }
-            })
-            const sql2 = 'INSERT INTO `Like` (`Tutee_userName`,`Tutor_userName`) VALUES (?,?)'
-            const data2 = [user.userName,tutorName]
-            db.query(sql2, data2, (err2,rows2)=>{
-                if(err2){
-                    res.status(400).send({msg:'fail'})
-                    console.log(err2)
-                }else{
-                    res.status(200).send({msg:'success'})
-                    console.log(rows2)
-                }
-            })
-        }
-    })
-
-})
+router.patch('/like',authMiddleware, like)
 
 // unlike
-router.patch('/unlike',authMiddleware,(req,res)=>{
-    const userName = res.locals.user.userName
-    console.log(req.body)
-    const {tutorName} = req.body;
-    const sql1 =  'UPDATE Tutor SET `like` = `like` - 1 WHERE userName=?'
-    db.query(sql1,tutorName,(err,rows1)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.log('success')
-        }
-    })
-    const sql2 = 'DELETE FROM `Like` WHERE Tutee_userName=? AND Tutor_userName=?'
-    // DELETE FROM [Table명] WHERE [Field명] = [조건 값]
-    const data2 = [userName,tutorName]
-    db.query(sql2, data2, (err2,rows2)=>{
-        if(err2){
-            res.status(400).send({msg:'fail'})
-            console.log(err2)
-        }else{
-            res.status(200).send({msg:'success'})
-            console.log(rows2)
-        }
-    })
-})
-router.get('/isLike', (req,res)=>{
-    const sql  = 'SELECT * FROM Like '
-
-
-})
-
-
-
-
+router.patch('/unlike',authMiddleware, unlike)
 
 // getPopularTutor  12개
-router.get('/getPopularTutor',(req,res)=>{
-    const sql = 'SELECT userId,userName,userEmail,isTutor,userProfile,tag,contents,startTime,endTime,comment,language1,language2,language3,`like` FROM `Tutor` ORDER BY `like` DESC'
-    db.query(sql,(err,data)=>{
-        if(err){
-            console.log(err)
-            res.status(400).send({msg:'fail'})
-        }else{
-            // console.log(data)
-            res.status(200).send({msg:'success', data})
-        }
-    })
-})
+router.get('/getPopularTutor', getPopularTutor)
+
 // getTutor
-router.get('/getTutor',(req,res)=>{
-    const sql = 'SELECT userId,userName,userEmail,isTutor,userProfile,tag,contents,startTime,endTime,comment,language1,language2,language3,`like` FROM Tutor' 
-    db.query(sql,(err,data)=>{
-        if(err){
-            console.log(err)
-            res.status(400).send({msg:'fail'})
-        }else{
-            // console.log(data)
-            res.status(200).send({msg:'success',data})
-        }
-    })
-})
-
-
-
+router.get('/getTutor', getTutor)
 
 // getTutor with keyword
-router.get('/getTutorTag/', (req,res)=>{
-    const {keyword}= req.query
-    const data  = `%${keyword}%`
-    const dataArr= [data,data,data,data]
-    const sql =
-        'SELECT userId,userName,userEmail,isTutor,userProfile,tag,contents,startTime,endTime,comment,language1,language2,language3,`like` FROM Tutor WHERE userName LIKE ? OR tag LIKE ? OR comment LIKE ? OR contents LIKE ?;'
-    db.query(sql, dataArr, (err,datas)=>{
-        if(err){
-            console.log(err)
-        }else{
-            res.status(200).send(datas)
-        }
-    })
-
-})
+router.get('/getTutorTag/', getTutorTag)
 
 // getTag 튜터마다 갖고 있는 태그값들 랜덤으로 뿌려주기
-router.get('/getTag', (req,res)=>{
-    const sql = 'SELECT tag FROM Tutor '
-    db.query(sql,(err,data)=>{
-        if(err) console.log(err)
-        else{
-            // console.log(data)
-            let arr =[]
-            for (let x of data){
-                arr.push(x.tag)
-            }
-            // tag값 하나씩 잘라주기
-            let arr2 = arr.join(',').split(',')
-            let arr3 =[]
-            // arr2 값들 하나씩 trim해서 새배열에 넣어주기
-            for(let x of arr2){
-                arr3.push(x.trim())
-                }   
-            // 배열내 인자들 shuffle함수
-            function shuffle(array) {
-                array.sort(() => Math.random() - 0.5);
-                }
-            // shuffle
-            shuffle(arr3)
-            // 배열내 중복값 제거
-            const arr4 = arr3.filter((element, index) => {
-                return arr3.indexOf(element) === index;
-            });
-            // 배열 내 length=0 인 인자들 제거
-            const arr5 = arr4.filter(el => el.length>0)
-            // 배열에서 20개까지만 추출
-            const arr6 = arr5.slice(0,20)
-            
-            res.status(200).send(arr6)
-        }
-    })
-})
-
-
-
-
-
+router.get('/getTag', getTag)
 
 // 유저상세페이지
-router.get('/getUserDetail/', (req,res)=>{
-    const{userName,isTutor} = req.query
-    if(isTutor==='1'){
-        const sql1 = 'SELECT userId,userName,userEmail,isTutor,userProfile,tag,contents,startTime,endTime,comment,language1,language2,language3,`like` FROM `Tutor` WHERE userName=?'
-        db.query(sql1, userName, (err,data)=>{
-            if(err){
-                console.log(err)
-                res.status(400).send({msg:'fail'})
-            }else{
-                res.status(200).send({msg:'success',data})
-            }
-        })
-    }else if(isTutor==='0'){
-        const sql2 = 'SELECT userId,userName,userEmail,isTutor,userProfile,tag,contents,startTime,endTime,comment,language1,language2,language3 FROM `Tutee` WHERE userName=?'
-        db.query(sql2, userName, (err,data)=>{
-            if(err){
-                console.log(err)
-                res.status(400).send({msg:'fail'})  
-            }else{
-                res.status(200).send({msg:'success', data})
-            }
-        })
-    }
-})
+router.get('/getUserDetail/', getUserDetail)
 // 좋아요한 선생님리스트, 자신을 좋아요한 학생리스트
-router.get('/getLikeList',authMiddleware,(req,res)=>{
-    const user = res.locals.user
-    if(user.isTutor===1){
-     
-        const sql = 'SELECT T.userName, T.userProfile FROM `Tutee` T LEFT OUTER JOIN `Like` L ON  T.userName = L.Tutee_userName WHERE L.Tutor_userName=?;'
-        db.query(sql,user.userName,(err,data)=>{
-            if(err){
-                console.log(err)
-                res.status(400).send({msg:'fail'})
-            }else{
-                res.status(200).send(data)
-            }
-        })    
-    }else if(user.isTutor===0){
-        const sql = 'SELECT T.userName, T.userProfile FROM `Tutor` T LEFT OUTER JOIN `Like` L ON  T.userName = L.Tutor_userName WHERE L.Tutee_userName=?';
-        db.query(sql,user.userName,(err,data)=>{
-            if(err){
-                console.log(err)
-                res.status(400).send({msg:'fail'})
-            }else{
-                res.status(200).send(data)
-            }
-        }) 
-    }
-})
+router.get('/getLikeList',authMiddleware, getLikeList)
 
-router.get('/isLike/:tutorName', authMiddleware,(req,res)=>{
-    
-    const userName = res.locals.user.userName
-    const isTutor = res.locals.user.isTutor
-    if(isTutor === 1){
-        res.status(400).send({msg:'error'})
-        return;
-    }
-    const {tutorName} = req.params
-    const sql0 = 'SELECT * FROM `Like` WHERE Tutee_userName=? and Tutor_userName=?'
-    const answerData = [ userName, tutorName ]
-    db.query(sql0, answerData, (err,data)=>{
-        console.log(data)
-        if(err){
-            console.log(err)
-            res.status(400).send({msg:"fail"})
-        }else if (data.length){
-            
-            res.status(200).send({isLike:true})
-        }else if (!data.length){
-            res.status(200).send({isLike:false})
-        }
-    })
-})
+// isLike : 좋아요 리스트에 추가되어있는지 판별해주는 API
+router.get('/isLike/:tutorName', authMiddleware, isLike)
 
 
 
